2eaf7b6aee86c55930f9b0e978b078d51374cea9
문소정 <82575174+Elisha0510@users.noreply.github.com>
2024-01-28 15:37:10 +0900

    [programmers] 구명보트 (Time: 12.12ms, Memory: 10.8MB) - by Code-Vault

---
+++ programmers/[42885] 구명보트/python/42885.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+def solution(people, limit):
+    cnt = 0
+    people.sort(reverse=True)
+    dq = deque(people)
+    
+    while(1):
+        if(len(dq)>=2):
+            if(dq[0] + dq[-1] <= limit):
+                cnt+=1
+                dq.popleft()
+                dq.pop()
+            else: 
+                cnt+=1
+                dq.popleft() 
+        elif(len(dq) == 1):
+            dq.pop()
+            cnt += 1
+            break
+        elif(len(dq) == 0):
+            break
+
+    return cnt
+        
+    
---
+++ programmers/[42885] 구명보트/python/README.md
@@ -0,0 +1,8 @@
+# [42885] 구명보트
+
+[문제 출처](https://school.programmers.co.kr/learn/courses/30/lessons/42885)
+
+## 성능
+
+- Time: 12.12ms
+- Memory: 10.8MB
