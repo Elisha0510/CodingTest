f2f304af699245679d25cdafd8d30a7778b11aa5
문소정 <82575174+Elisha0510@users.noreply.github.com>
2024-01-28 13:12:19 +0900

    [programmers] 점프와 순간 이동 (Time: 0.03ms, Memory: 10.4MB) - by Code-Vault

---
+++ programmers/[12980] 점프와 순간 이동/python/12980.py
@@ -0,0 +1,16 @@
+def solution(n):
+    cnt = 0 
+    answer = find_minimum(n, cnt)
+    return answer
+    
+def find_minimum(n, cnt):
+    if(n%2 == 0 and n != 2):
+        return find_minimum(int(n/2), cnt)
+    elif(n%2 == 1 and n != 1):
+        cnt += 1
+        return find_minimum(n-1, cnt)
+    elif(n == 2):
+        return find_minimum(int(n/2), cnt)
+    elif(n == 1):
+        cnt += 1
+        return cnt
---
+++ programmers/[12980] 점프와 순간 이동/python/README.md
@@ -0,0 +1,8 @@
+# [12980] 점프와 순간 이동
+
+[문제 출처](https://school.programmers.co.kr/learn/courses/30/lessons/12980)
+
+## 성능
+
+- Time: 0.03ms
+- Memory: 10.4MB
