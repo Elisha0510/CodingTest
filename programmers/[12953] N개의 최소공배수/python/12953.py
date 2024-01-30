d4ccaed82c9844d8a51ff3ed642243b9cf218d39
문소정 <82575174+Elisha0510@users.noreply.github.com>
2024-01-30 22:26:06 +0900

    [programmers] N개의 최소공배수 (Time: 0.02ms, Memory: 10.3MB) - by Code-Vault

---
+++ programmers/[12953] N개의 최소공배수/python/12953.py
@@ -0,0 +1,28 @@
+def solution(arr):
+    arr.sort()
+    
+    mul, answer = 1,1
+    for i in arr:
+        mul *= i
+
+    result = arr[0]
+    for i in range(1, len(arr)):
+        g = max_number(result, arr[i])
+        result = (result / g) * arr[i]
+    
+    return result
+
+def max_number(a, b):
+    while(1):
+        div = a%b
+        if(div == 0):
+            break
+        else:
+            a = b
+            b = div
+    
+    return b
+        
+            
+            
+            
---
+++ programmers/[12953] N개의 최소공배수/python/README.md
@@ -0,0 +1,8 @@
+# [12953] N개의 최소공배수
+
+[문제 출처](https://school.programmers.co.kr/learn/courses/30/lessons/12953)
+
+## 성능
+
+- Time: 0.02ms
+- Memory: 10.3MB
