c7d3990cd17e2a5674bb4a2da1931013abaaf244
문소정 <82575174+Elisha0510@users.noreply.github.com>
2024-02-20 21:13:30 +0900

    [programmers] [1차] 뉴스 클러스터링 (Time: 0.81ms, Memory: 10.4MB) - by Code-Vault

---
+++ programmers/[17677] [1차] 뉴스 클러스터링/python/17677.py
@@ -0,0 +1,30 @@
+from collections import Counter
+
+def solution(str1, str2):
+    str1 = str1.lower()
+    str2 = str2.lower()
+    
+    strArr1 = []
+    strArr2 = []
+    
+    for i in range(len(str1)-1):
+        if(str1[i].isalpha() and str1[i+1].isalpha()):
+            strArr1.append(str1[i]+str1[i+1])
+        
+    for i in range(len(str2)-1):
+        if(str2[i].isalpha() and str2[i+1].isalpha()):
+            strArr2.append(str2[i]+str2[i+1])
+            
+    c1 = Counter(strArr1)
+    c2 = Counter(strArr2)
+    
+    equalLength = len(list((c1 & c2).elements()))
+    sumLength = len(list((c1 | c2).elements()))
+    
+    if (sumLength == 0 and equalLength == 0):
+        return 65536
+    else:
+        return int(equalLength / sumLength * 65536)
+
+    
+        
---
+++ programmers/[17677] [1차] 뉴스 클러스터링/python/README.md
@@ -0,0 +1,8 @@
+# [17677] [1차] 뉴스 클러스터링
+
+[문제 출처](https://school.programmers.co.kr/learn/courses/30/lessons/17677)
+
+## 성능
+
+- Time: 0.81ms
+- Memory: 10.4MB
