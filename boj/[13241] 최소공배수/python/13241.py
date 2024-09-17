cc646b099f367d679a46872038cef15856b21a36
문소정 <82575174+Elisha0510@users.noreply.github.com>
2024-09-17 15:56:26 +0900

    [boj] 최소공배수 (Time: 32ms, Memory: 31.12MB) - by Code-Vault

---
+++ boj/[13241] 최소공배수/python/13241.py
@@ -0,0 +1,19 @@
+import sys
+
+def gcd(a, b):
+    c = -1
+    while 1:
+        c = a % b
+        if not c:
+            return b
+        a = b
+        b = c
+
+arr = list(map(int, sys.stdin.readline().strip().split()))
+
+if arr[0] >= arr[1]:
+    number = gcd(arr[0], arr[1])
+else:
+    number = gcd(arr[1], arr[0])
+
+print(arr[0]*arr[1]//number)
---
+++ boj/[13241] 최소공배수/python/README.md
@@ -0,0 +1,8 @@
+# [13241] 최소공배수
+
+[문제 출처](https://www.acmicpc.net/problem/13241)
+
+## 성능
+
+- Time: 32ms
+- Memory: 31.12MB
