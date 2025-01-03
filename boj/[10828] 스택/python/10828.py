fe422a7daac7a8a8e003345c5a2811505715d54f
문소정 <82575174+Elisha0510@users.noreply.github.com>
2025-01-03 21:26:00 +0900

    [boj] 스택 (Time: 48ms, Memory: 33.432MB) - by Code-Vault

---
+++ boj/[10828] 스택/python/10828.py
@@ -0,0 +1,24 @@
+import sys
+
+n =  int(input())
+
+stack = []
+for i in range(n):
+    arr = list(sys.stdin.readline().strip().split())
+    
+    if len(arr) >= 2 and arr[0] == "push":
+        stack.append(arr[1])
+    elif arr[0] == "pop":
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack.pop())
+    elif arr[0] == "size":
+        print(len(stack))
+    elif arr[0] == "empty":
+        print(1) if len(stack) == 0 else print(0)
+    elif arr[0] == "top":
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[-1])
---
+++ boj/[10828] 스택/python/README.md
@@ -0,0 +1,8 @@
+# [10828] 스택
+
+[문제 출처](https://www.acmicpc.net/problem/10828)
+
+## 성능
+
+- Time: 48ms
+- Memory: 33.432MB
