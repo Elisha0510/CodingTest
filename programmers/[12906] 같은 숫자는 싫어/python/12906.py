4323a6b67d4fa0f1cb14413a989160c0f67a93e1
문소정 <82575174+Elisha0510@users.noreply.github.com>
2025-09-18 11:15:22 +0900

    [programmers] 같은 숫자는 싫어 (Time: 113.83ms, Memory: 27.2MB) - by Code-Vault

---
+++ programmers/[12906] 같은 숫자는 싫어/python/12906.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = []
+    for a in arr:
+        if len(answer) == 0:
+            answer.append(a)
+        else:
+            if answer[-1] != a:
+                answer.append(a)
+                
+    return answer
---
+++ programmers/[12906] 같은 숫자는 싫어/python/README.md
@@ -0,0 +1,8 @@
+# [12906] 같은 숫자는 싫어
+
+[문제 출처](https://school.programmers.co.kr/learn/courses/30/lessons/12906)
+
+## 성능
+
+- Time: 113.83ms
+- Memory: 27.2MB
